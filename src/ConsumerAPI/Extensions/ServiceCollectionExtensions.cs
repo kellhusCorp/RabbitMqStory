e8e94bc897fa94d27817b1a515f1eb0ac8d75426
kellhusCorp <alkan_7@mail.ru>
2023-05-08 17:20:58 +0400

    Закинул расширение для rabbit

---
+++ src/ConsumerAPI/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Options;
+using RabbitMQ.Client;
+
+namespace ConsumerAPI.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static void AddRabbitMqConnectionFactory(this IServiceCollection services)
+    {
+        services.AddSingleton<IConnectionFactory, ConnectionFactory>(provider =>
+        {
+            var settings = provider.GetRequiredService<IOptionsMonitor<RabbitMqSettings>>().CurrentValue;
+
+            return new ConnectionFactory
+            {
+                HostName = settings.Host,
+                UserName = "user",
+                Password = "password",
+                Port = 5672
+            };
+        });
+    }
+}
